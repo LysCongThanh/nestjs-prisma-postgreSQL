08318cc4af845ae8ee053cd460ab39cbc3e67689
LysCongThanh <congthanh1770@gmail.com>
2024-03-26 15:18:43 +0700

    test cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
-import { CorsModule } from "./cors/cors.module";
+import * as cors from 'cors';
 
 @Module({
   imports: [
-    CorsModule,
     AuthModule,
     PrismaModule,
     ConfigModule.forRoot({
@@ -16,4 +15,16 @@ import { CorsModule } from "./cors/cors.module";
     UserModule,
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer
+      .apply(
+        cors({
+          origin: 'https://youtube.com',
+          methods: 'GET,POST,PUT,DELETE,PATCH',
+          allowedHeaders: ['content-type'],
+        }),
+      )
+      .forRoutes('*');
+  }
+}
