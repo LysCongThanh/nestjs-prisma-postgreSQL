393d0f34243845658f883d4a0a0a891f0a502dfd
LysCongThanh <congthanh1770@gmail.com>
2024-03-30 17:47:43 +0700

    update src

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: false,
+    origin: 'http://localhost:3000',
     credentials: true,
   });
   app.listen(process.env.PORT);
